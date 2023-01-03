76a78ca96ed67e521b8faf971b886b66417e371a
Prasiddha Shrestha <prasiddhastha7@gmail.com>
2023-01-03 22:12:39 +0545

    Add project files.

---
+++ DemoForLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CsharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var i=1;i<=10;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine(" ");
+            for(var i=10;i>=1;i--)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
+        }
+    }
+}
